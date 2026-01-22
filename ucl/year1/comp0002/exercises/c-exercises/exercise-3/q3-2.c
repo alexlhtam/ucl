2ddd4171c034ed6fa522ee5d133da17b338e7230
alex@MBPro14 <alexlhtam@gmail.com>
2026-01-22 15:32:44 +0000

    rewrote q3-2

--- ucl/year1/comp0002/exercises/c-exercises/exercise-3/q3-2.c
+++ ucl/year1/comp0002/exercises/c-exercises/exercise-3/q3-2.c
@@ -1,31 +1,32 @@
-// write a program that defines and calls a function to raise an integer to another positive integer
-// provide a version that uses a loop and another that uses recursion
+// Write a program that defines and calls a function to raise an integer to a positive integer power (e.g., x^y). 
+// Provide a version of the function that uses a loop and another that uses recursion.
 
 # include <stdio.h>
 
+int r_power(int b, int exp) {
+    if (exp == 0) return 1;
 
-int iterative_power(int base, int power) {
-    int x = base;
-    for (int i = 0; i < power; i++) {
-        x *= base;
-    }
-
-    return x;
+    return b * r_power(b, exp - 1);
 }
 
-int recursive_power(int base, int power) {
-    if (power == 0) {
-        return 1;
-    } else {
-        return base * recursive_power(base, power - 1);
+int i_power(int b, int exp) {
+    int res = 1;
+    for (int i = 0; i < exp; i++) {
+        res *= b;
     }
+
+    return res;
 }
+
 
 int main(void) {
+    int base, exponent;
 
-    printf("3^3 iteratively: %d\n", iterative_power(3, 3));
+    printf("Enter base followed by the exponent (ints): ");
+    scanf("%i %i", &base, &exponent);
 
-    printf("2^4 recursively: %d\n", recursive_power(2, 4));
+    printf("recursive call of %i ^ %i = %i\n", base, exponent, r_power(base, exponent));
+    printf("iterative call of %i ^ %i = %i\n", base, exponent, i_power(base, exponent));
 
     return 0;
 }
